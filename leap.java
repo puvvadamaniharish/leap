ef041d3582359fcd2e96191b1acfb21db561179e
puvvadamaniharish <41737685+puvvadamaniharish@users.noreply.github.com>
2018-07-27 23:27:37 +0530

    Create leap.java

---
+++ leap.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner g = new Scanner (System.in);
+		int f = g.nextInt();
+		 if ( f % 4 == 0)
+		   System.out.println("yes");
+		 else 
+		     System.out.println("no");
+		 
+	}
+}
